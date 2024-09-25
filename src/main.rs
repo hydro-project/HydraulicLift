b8d514580c46391d09f8a9219ca54d7536fe749a
Ryan Alameddine <rhalameddin@gmail.com>
2024-09-24 20:10:31 -0700

    moved segment traversal into visitor for nexted statements

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,25 @@ use std::{collections::HashSet, convert::identity, hash::Hash, mem::take, path::
 use proc_macro2::TokenStream;
 use quote::{quote, ToTokens};
 use syn::{
-    parse_quote, parse_str, punctuated::Punctuated, visit::{self, Visit}, Block, Expr, ExprAwait, ExprClosure, ExprPath, ExprTuple, Ident, Local, LocalInit, Pat, PatIdent, PatPath, Path, Stmt
+    parse_quote, parse_str, punctuated::Punctuated, token::Brace, visit::{self, Visit}, Block, Expr, ExprAwait, ExprClosure, ExprPath, ExprTuple, Ident, Local, LocalInit, Pat, PatIdent, PatPath, Path, Stmt
 };
 
+#[derive(Clone)]
 enum RawSegment {
     Await(Vec<Stmt>),
     End(Vec<Stmt>),
 }
 
+
 impl RawSegment {
-    fn iter(&self) -> std::slice::Iter<'_, Stmt> {
-        let v = match self {
-            Self::Await(v) => v,
-            Self::End(v) => v,
-        };
-        v.iter()
+    fn into_block(self) -> Block {
+        Block {
+            brace_token: Default::default(),
+            stmts: match self {
+                Self::Await(v) => v,
+                Self::End(v) => v,
+            }
+        }
     }
 
     fn into_segment(self, inputs: Vec<Ident>, outputs: Vec<Ident>) -> Segment {
@@ -70,17 +74,17 @@ fn is_await(stmt: &Stmt) -> bool {
     )
 }
 
-fn register_statement_locals(mut locals: HashSet<Ident>, stmt: &Stmt) -> HashSet<Ident> {
-    // the local defined this statement
-    if let Some(defined_local) = get_defined_local(stmt) {
-        locals.remove(&defined_local); // TODO: could be many!! need to visit
-    }
-    // the locals needed by this statement
-    let needed_locals = get_needed_locals(stmt);
-    locals.extend(needed_locals);
+// fn register_statement_locals(mut locals: HashSet<Ident>, stmt: &Stmt) -> HashSet<Ident> {
+//     // the local defined this statement
+//     if let Some(defined_local) = get_defined_local(stmt) {
+//         locals.remove(&defined_local); // TODO: could be many!! need to visit
+//     }
+//     // the locals needed by this statement
+//     let needed_locals = get_needed_locals(stmt);
+//     locals.extend(needed_locals);
 
-    locals
-}
+//     locals
+// }
 
 fn get_defined_local(stmt: &Stmt) -> Option<Ident> {
     match stmt {
@@ -97,29 +101,48 @@ struct LocalVisitor {
 }
 
 impl LocalVisitor {
-    fn new() -> LocalVisitor {
-        LocalVisitor {
-            idents: HashSet::new(),
-        }
+    fn visit_raw_segment(idents: Vec<Ident>, raw_segment: RawSegment) -> Vec<Ident> {
+        let mut visitor = LocalVisitor {
+            idents: idents.into_iter().collect()
+        };
+        visitor.visit_block(&raw_segment.into_block());
+        visitor.idents.into_iter().collect()
     }
 }
 
 impl<'ast> Visit<'ast> for LocalVisitor {
-    fn visit_expr_path(&mut self, p: &'ast syn::ExprPath) {
-        if let Some(ident) = p.path.get_ident() {
+    // visit statements in reverse
+    fn visit_block(&mut self, block: &'ast syn::Block) {
+        for stmt in block.stmts.iter().rev() {
+            self.visit_stmt(stmt);
+        }
+    }
+
+    // Add visited locals
+    fn visit_expr_path(&mut self, exprpath: &'ast syn::ExprPath) {
+        if let Some(ident) = exprpath.path.get_ident() {
             self.idents.insert(ident.clone());
         }
 
         // visit nested?
-        visit::visit_expr_path(self, p);
+        visit::visit_expr_path(self, exprpath);
+    }
+
+    fn visit_local(&mut self, local: &'ast syn::Local) {
+        if let Pat::Ident(PatIdent { ident, ..}) = &local.pat {
+            self.idents.remove(ident);
+        }
+        
+        // visit nested?
+        visit::visit_local(self, local);
     }
 }
 
-fn get_needed_locals(stmt: &Stmt) -> HashSet<Ident> {
-    let mut visitor = LocalVisitor::new();
-    visitor.visit_stmt(stmt);
-    visitor.idents
-}
+// fn get_needed_locals(stmt: &Stmt) -> HashSet<Ident> {
+//     let mut visitor = LocalVisitor::new();
+//     visitor.visit_stmt(stmt);
+//     visitor.idents
+// }
 
 fn main() {
     let block: Block = parse_str(
@@ -137,8 +160,9 @@ fn main() {
         let asdf = unused + 1;
     }
 
-    let e = c.await;
-    z
+    let e = d.await;
+    let out = z + 2;
+    out
 }
 ",
     )
@@ -163,29 +187,24 @@ fn main() {
     }
     raw_segs.push(RawSegment::End(cur_segment));
 
-    // for each segment, the set of local variables which need to be passed on
+    // parse the raw segments into segments (adding local variable input/output information)
     let segments = raw_segs
         .into_iter()
         .rev() // walk backwards through segments
         // scan over the segments backwards, tracking the needed set of locals in each segment
         .scan(Vec::new(), |state, raw_seg| {
             let outputs = take(state);
-            let init = outputs.iter().cloned().collect::<HashSet<_>>();
-            let inputs = raw_seg
-                .iter()
-                .rev()
-                .fold(init, register_statement_locals)
-                .into_iter()
-                .collect::<Vec<_>>();
+            let inputs = LocalVisitor::visit_raw_segment(outputs.clone(), raw_seg.clone());
             *state = inputs.clone();
 
             Some(raw_seg.into_segment(inputs, outputs))
         })
         .collect::<Vec<_>>()
         .into_iter()
-        .rev()
+        .rev() //undo the reverse at the beginning
         .collect::<Vec<_>>();
 
+    // concatenate all segments
     let segment_tokens = segments
         .into_iter()
         .map(ToTokens::into_token_stream)
